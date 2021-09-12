6c166a453f49be74ab5264c97013480d70ca7549
tcgfh <robert.kan@gmail.com>
2021-09-12 13:55:28 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+/**
+* Entry point of the app
+* @return {object} react component representing the main app
+*/
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
